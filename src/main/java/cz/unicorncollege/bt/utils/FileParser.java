89c6ffe1cfdd48f51361c61b3e8893c4e89cc447
Tadeas Firich <tadeasfirich696@gmail.com>
2017-12-19 21:49:05 +0100

    fix wrong saving reservations

--- src/main/java/cz/unicorncollege/bt/utils/FileParser.java
+++ src/main/java/cz/unicorncollege/bt/utils/FileParser.java
@@ -273,7 +273,6 @@ public class FileParser {
 					if (room.getReservations().isEmpty()) {
 						hasNoReservations++;
 						//TODO: Dodelat vyjímku
-						break;
 					} else {
 						for (Reservation meetingReservation : room.getReservations()) {
 							Element reservation = doc.createElement("reservation");
